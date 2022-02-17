1fb89a41f9816df11dce89b03fa7698f50c0a94b
Zakaria6789 <jakaria.hossain6363@gmail.com>
2022-02-17 20:36:50 +0600

    error solve ok

--- js/money.js
+++ js/money.js
@@ -10,15 +10,25 @@ const balance = document.getElementById('balance');
 
 
 document.getElementById('calculate').addEventListener('click', function () {
+    const incomeAmount = income.value;
     const foodCost = food.value;
     const rentCost = rent.value;
     const clothesCost = clothes.value;
+    const totalCost = parseInt(foodCost) + parseInt(rentCost) + parseInt(clothesCost);
 
-    if (foodCost >= 0 && rentCost >= 0 && clothesCost >= 0) {
-        totalExpenses.innerText = parseInt(foodCost) + parseInt(rentCost) + parseInt(clothesCost);
-        balance.innerText = parseInt(income.value) - parseInt(totalExpenses.innerText);
+    if (totalCost > income.value) {
+        document.getElementById('over-expenses').style.display = 'block';
+        document.getElementById('string-massage').style.display = 'none';
+        document.getElementById('negative-number-massage').style.display = 'none';
     }
-    else if (foodCost < 0 || rentCost < 0 || clothesCost < 0) {
+    else if (foodCost >= 0 && rentCost >= 0 && clothesCost >= 0 && incomeAmount >= 0) {
+        totalExpenses.innerText = totalCost;
+        balance.innerText = parseInt(income.value) - parseInt(totalExpenses.innerText);
+        document.getElementById('negative-number-massage').style.display = 'none';
+        document.getElementById('string-massage').style.display = 'none';
+    }
+
+    else if (foodCost < 0 || rentCost < 0 || clothesCost < 0 || incomeAmount < 0) {
         document.getElementById('negative-number-massage').style.display = 'block';
         document.getElementById('string-massage').style.display = 'none';
     }
@@ -35,6 +45,13 @@ const saveInput = document.getElementById('save-input');
 const remaining = document.getElementById('remaining');
 
 document.getElementById('save').addEventListener('click', function () {
-    savingAmount.innerText = (balance.innerText * saveInput.value) / 100;
-    remaining.innerText = balance.innerText - savingAmount.innerText;
+    if (saveInput.value > 100) {
+        document.getElementById('over-balance').style.display = 'block';
+
+    }
+    else if (saveInput.value >= 0) {
+        savingAmount.innerText = (balance.innerText * saveInput.value) / 100;
+        remaining.innerText = balance.innerText - savingAmount.innerText;
+        document.getElementById('over-balance').style.display = 'none';
+    }
 })
