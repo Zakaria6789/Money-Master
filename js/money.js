98507da94f0b703fd4141bd51bb7dc4fb0435028
Zakaria6789 <jakaria.hossain6363@gmail.com>
2022-02-17 16:37:33 +0600

    error solve something

--- js/money.js
+++ js/money.js
@@ -6,9 +6,27 @@ const clothes = document.getElementById('clothes');
 const totalExpenses = document.getElementById('total-expenses');
 const balance = document.getElementById('balance');
 
+// input-value:
+
+
 document.getElementById('calculate').addEventListener('click', function () {
-    totalExpenses.innerText = parseInt(food.value) + parseInt(rent.value) + parseInt(clothes.value);
-    balance.innerText = parseInt(income.value) - parseInt(totalExpenses.innerText);
+    const foodCost = food.value;
+    const rentCost = rent.value;
+    const clothesCost = clothes.value;
+
+    if (foodCost >= 0 && rentCost >= 0 && clothesCost >= 0) {
+        totalExpenses.innerText = parseInt(foodCost) + parseInt(rentCost) + parseInt(clothesCost);
+        balance.innerText = parseInt(income.value) - parseInt(totalExpenses.innerText);
+    }
+    else if (foodCost < 0 || rentCost < 0 || clothesCost < 0) {
+        document.getElementById('negative-number-massage').style.display = 'block';
+        document.getElementById('string-massage').style.display = 'none';
+    }
+    else {
+        document.getElementById('string-massage').style.display = 'block';
+        document.getElementById('negative-number-massage').style.display = 'none';
+    }
+
 })
 
 // Money Saving:
